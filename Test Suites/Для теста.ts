6b316bd73e169993ac879349aa72dfabd6ee8456
Zhukov Pavel <Zhukovflycode@gmail.com>
2022-11-17 16:01:53 +0300

    Тесты CI/CD

---
+++ Test Suites/Для теста.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Для теста</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8a94281a-9666-4bc0-bdc9-0ebea744715d</testSuiteGuid>
+   <testCaseLink>
+      <guid>b6999abd-5385-44e8-838f-c72a71b6e2da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Авторизация</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
