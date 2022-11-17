a3c3fdd5a98914ab27c56b16e19f046661966cb7
Zhukov Pavel <Zhukovflycode@gmail.com>
2022-11-17 16:38:50 +0300

    Для теста CI CD

--- Test Suites/Для теста сд.ts
+++ Test Suites/Для теста сд.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Для теста</name>
+   <name>Для теста сд</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
