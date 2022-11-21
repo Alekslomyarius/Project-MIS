6992c0148070553fc5fe588a382b3e14a9a75cc1
Zhukov Pavel <Zhukovflycode@gmail.com>
2022-11-21 12:37:59 +0300

    Проверка отправки отчета на email

--- Test Suites/Create patient in consultations.ts
+++ Test Suites/Create patient in consultations.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Создание пациента в приеме</name>
+   <name>Create patient in consultations</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
